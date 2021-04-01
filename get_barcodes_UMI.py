96c409a9b9395daae1e7db26ff6d277146c9bd83
Weiyi Li <52840573+Li-WY@users.noreply.github.com>
2021-04-01 10:14:11 -0700

    Update get_barcodes_UMI.py

--- get_barcodes_UMI.py
+++ get_barcodes_UMI.py
@@ -5,10 +5,9 @@ outfile2=open(sys.argv[1]+'_'+"R",'w')
 
 file_raw_barcodes=open(sys.argv[1],'r')
 for each_line in file_raw_barcodes:
-	for each_line in file_raw_barcodes:
-		L=each_line.strip().split(',')
-		if len(L) ==5:
-			outfile1.write(L[1]+','+L[4]+'\n')
-			outfile2.write(L[0]+','+L[4]+'\n')
+	L=each_line.strip().split(',')
+	if len(L) ==5:
+		outfile1.write(L[1]+','+L[4]+'\n')
+		outfile2.write(L[0]+','+L[4]+'\n')
 outfile1.close()
 outfile2.close()
